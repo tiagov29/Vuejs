d232795b8cddb325f7664b91a689afae946e611c
tiagov29 <santiagovalenciav29@gmail.com>
2021-11-30 19:58:46 -0500

    beginning project with vue

---
+++ vueProject/test/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class= "container">
+
+  <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+    }
+
+    .btn {
+      display: inline-block;
+      background: #000;
+      color: #fff;
+      border: none;
+      padding: 10px 20px;
+      margin: 5px;
+      border-radius: 5px;
+      cursor: pointer;
+      text-decoration: none;
+      font-size: 15px;
+      font-family: inherit;
+      }
+
+
+</style>
